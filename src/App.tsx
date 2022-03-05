b752f19586cce128b8a8a7d93c08347fd40aa451
harrytang <harry@powerkernel.com>
2022-03-05 15:44:47 +0200

    chore: new events

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import Event from './components/Event';
 
 const events = [
   {
-    date: 'Tue 01 Mar 2022, 20:30 - 22:30',
+    date: 'Tue 08 Mar 2022, 20:30 - 22:30',
     location: 'eSport',
     form: 'https://forms.gle/bsPuAboVBoiJAtW89',
     view: 'https://docs.google.com/forms/d/e/1FAIpQLSfDSN2M6W6BUjL72rGgl2WnbvKXFBoYYaCgezCH3-m8FYgqLQ/viewanalytics',
   },
-  // {
-  //   date: 'Thu 17 Feb 2022, 20:30 - 22:30',
-  //   location: 'eSport',
-  //   form: 'https://forms.gle/YMCaPVseYmhS1caGA',
-  //   view: 'https://docs.google.com/forms/d/e/1FAIpQLSfn4xSq_6cZXcnT-ZtczJpmaUaKe4FStNBeGaxjoIH8SubvVA/viewanalytics',
-  // },
+  {
+    date: 'Tue 10 Mar 2022, 20:30 - 22:30',
+    location: 'UniSport Otaniemi *',
+    form: 'https://forms.gle/YMCaPVseYmhS1caGA',
+    view: 'https://docs.google.com/forms/d/e/1FAIpQLSfn4xSq_6cZXcnT-ZtczJpmaUaKe4FStNBeGaxjoIH8SubvVA/viewanalytics',
+  },
   // {
   //   date: 'Thu 23 Dec 2021, 20:30 - 22:30',
   //   location: 'eSport',
@@ -31,6 +31,9 @@ function App() {
       {events.map((event, i) => {
         return <Event event={event} key={i} />;
       })}
+      <p>
+        <i>* Hosted by Lien</i>
+      </p>
     </div>
   );
 }
